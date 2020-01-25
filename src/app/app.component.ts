c74660708e2cc4c11830be6e2794fdb71ebe6bfa
Aiden Gonzalez <aidenlgonzalez2@gmail.com>
2020-01-24 23:41:38 -0500

    Angular map working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AgmCoreModule } from '@agm/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'web-hydrate';
+  lat = 40.427444;
+  lng = -86.916861;
+  zoom = 16;
+  minZoom = 15;
 }
