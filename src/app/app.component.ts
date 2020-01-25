e2bf44bcc632738cb5f13240529d5c1990b2d095
Aiden Gonzalez <aidenlgonzalez2@gmail.com>
2020-01-25 04:02:09 -0500

    Beef up map stuff
    Signed-off-by: Aiden Gonzalez <aidenlgonzalez2@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import { AgmCoreModule } from '@agm/core';
+import {compareNumbers} from '@angular/compiler-cli/src/diagnostics/typescript_version';
 
 @Component({
   selector: 'app-root',
@@ -8,13 +9,44 @@ import { AgmCoreModule } from '@agm/core';
 })
 export class AppComponent implements OnInit {
   title = 'web-hydrate';
-  lat = 40.427444;
-  lng = -86.916861;
-  zoom = 16;
-  minZoom = 15;
-  maxZoom = 30;
+  zoom: Zoom;
+  location: Location;
+  markers: Marker[];
 
-  ngOnInit(): void {
-
+  ngOnInit() {
+    this.location = {
+      lat: 40.427444,
+      lng: -86.916861
+    };
+    this.zoom = {
+      defzoom: 16,
+      minZoom: 15,
+      maxZoom: 30
+    };
+    this.markers = [];
+    this.addMarker(this.location);
   }
+
+  addMarker(location: Location) {
+    this.markers.push({
+      lat: location.lat,
+      lng: location.lng
+    });
+  }
+}
+
+interface Location {
+  lat: number;
+  lng: number;
+}
+
+interface Zoom {
+  defzoom: number;
+  minZoom: number;
+  maxZoom: number;
+}
+
+interface Marker {
+  lat: number;
+  lng: number;
 }
