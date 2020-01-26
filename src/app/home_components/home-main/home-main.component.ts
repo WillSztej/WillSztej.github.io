2234f22922c6845a0dd991c022b9ec49ebe71ff8
willsztej <williamsztejnberg@gmail.com>
2020-01-26 04:16:11 -0500

    Fixed Routing Bugs Changed Router Link Orientation

--- src/app/home_components/home-main/home-main.component.ts
+++ src/app/home_components/home-main/home-main.component.ts
@@ -6,14 +6,15 @@ import {Fountain} from 'src/app/fountain/fountain.model';
 import {FountainService} from 'src/app/fountain/fountain.service';
 import { AngularFireStorage, AngularFireStorageReference } from '@angular/fire/storage';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home-main',
   templateUrl: './home-main.component.html',
   styleUrls: ['./home-main.component.css']
 })
-export class HomeMainComponent implements OnInit {
 
+export class HomeMainComponent implements OnInit {
   title = 'web-hydrate';
   zoom: Zoom = {
     defzoom: 16,
@@ -21,18 +22,37 @@ export class HomeMainComponent implements OnInit {
     maxZoom: 30
   };
   location: Location = {
-    lat: 40.427444,
-    lng: -86.916861
+    lat: 40,
+    lng: -80
   };
+  userLocation = false;
   fountains: Fountain[];
   fountain: Fountain;
   markers: Marker[] = [];
   style = environment.mainMapStyle;
   isLoaded = false;
+  userMarkerTitle = 'Current Location';
+  icon = {
+    url: 'https://firebasestorage.googleapis.com/v0/b/web-hydrate.appspot.' +
+      'com/o/blue-dot.png?alt=media&token=01e35e36-25f3-4143-9b1c-300038f2864e',
+    scaledSize: {
+      width: 41,
+      height: 41
+    }
+  };
+
 
   constructor(private fountainService: FountainService,
               private fireStorage: AngularFireStorage,
-              public modalService: NgbModal) { }
+              public modalService: NgbModal) {
+    if (navigator) {
+      navigator.geolocation.getCurrentPosition( pos => {
+        this.location.lng = +pos.coords.longitude;
+        this.location.lat = +pos.coords.latitude;
+        this.userLocation = true;
+      });
+    }
+  }
 
   ngOnInit() {
     this.fountainService.getFountains().subscribe(data => {
@@ -48,7 +68,6 @@ export class HomeMainComponent implements OnInit {
       this.isLoaded = true;
     });
   }
-
   create(fountain: Fountain) {
     this.fountainService.createFountain(fountain);
   }
@@ -60,7 +79,6 @@ export class HomeMainComponent implements OnInit {
   delete(fountainID: string) {
     this.fountainService.deleteFountain(fountainID);
   }
-
   addMarker(latitude: number, longitude: number, name: string) {
     this.markers.push({
       lat: latitude,
@@ -77,6 +95,26 @@ export class HomeMainComponent implements OnInit {
       size: 'lg'
     });
   }
+}
+
+function calculateDistance(lat1, lon1, lat2, lon2) {
+  const R = 6371;
+  const dLat = deg2rad(lat2 - lat1);
+  const dLon = deg2rad(lon2 - lon1);
+  const a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+    Math.sin(dLon / 2 * Math.sin(dLon / 2))
+  ;
+
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  let d = R * c;
+  d *= 0.621371;
+  return d;
+}
+
+function deg2rad(deg) {
+  return deg * (Math.PI / 180);
 }
 
 interface Location {
