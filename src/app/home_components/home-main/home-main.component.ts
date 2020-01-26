c43bcf2ba89c511b20b7ac0af2f320b67d364215
willsztej <williamsztejnberg@gmail.com>
2020-01-26 07:30:51 -0500

    Fixed Routing Bugs Changed Router Link Orientation

--- src/app/home_components/home-main/home-main.component.ts
+++ src/app/home_components/home-main/home-main.component.ts
@@ -86,6 +86,24 @@ export class HomeMainComponent implements OnInit {
   delete(fountainID: string) {
     this.fountainService.deleteFountain(fountainID);
   }
+  deg2rad(deg) {
+    return deg * (Math.PI / 180);
+  }
+  distance(lat1: number, lon1: number, lat2: number, lon2: number) {
+    const R = 6371;
+    const dLat = this.deg2rad(lat2 - lat1);
+    const dLon = this.deg2rad(lon2 - lon1);
+    const a =
+      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+      Math.cos(this.deg2rad(lat1)) * Math.cos(this.deg2rad(lat2)) *
+      Math.sin(dLon / 2 * Math.sin(dLon / 2))
+    ;
+
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    let d = R * c;
+    d *= 0.621371;
+    return d;
+  }
 
   addMarker(latitude: number, longitude: number, name: string) {
     this.markers.push({
@@ -124,25 +142,6 @@ export class HomeMainComponent implements OnInit {
   }
 }
 
-function calculateDistance(lat1, lon1, lat2, lon2) {
-  const R = 6371;
-  const dLat = deg2rad(lat2 - lat1);
-  const dLon = deg2rad(lon2 - lon1);
-  const a =
-    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
-    Math.sin(dLon / 2 * Math.sin(dLon / 2))
-  ;
-
-  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-  let d = R * c;
-  d *= 0.621371;
-  return d;
-}
-
-function deg2rad(deg) {
-  return deg * (Math.PI / 180);
-}
 
 interface Location {
   lat: number;
